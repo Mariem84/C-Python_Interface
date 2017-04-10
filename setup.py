fc257338dbd8f79c3c63ba3531d8ebaef03905c8
Mariem84 <mariemkthiri3@gmail.com>
2017-04-10 11:09:30 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-
-
-example_module = Extension('_example', sources=['example.i', 'example.cpp'], swig_opts=['-c++'], library_dirs = ['/usr/lib'], libraries = ['python2.7', 'pthread', 'dl',  'util', 'm'], include_dirs = ['/usr/include/python2.7'])
-
-setup(name='example', ext_modules=[example_module], py_modules=["example"])
