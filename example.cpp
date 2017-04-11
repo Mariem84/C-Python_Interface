4cb9a5222b9960f565ad9a53ca386c7612b02d68
Mariem K <mariemkthiri3@gmail.com>
2017-04-11 14:43:25 +0200

    CMake

--- example.cpp
+++ example.cpp
@@ -6,12 +6,12 @@
 #include <vector>
 
 
-std::vector<double>& simul(Device d, Scenario s) {
+std::vector<double> simul(Device d, Scenario s) {
 	std::vector<double> t(32000);
 	for (int i=0; i<32000; i++){
 		t[i] = i;
 		}
-	std::vector<double>& a(32000); 
+	std::vector<double> a(32000); 
 	for (int i=0; i<32000; i++){
 		a[i] = std::exp(t[i]);
 		}
